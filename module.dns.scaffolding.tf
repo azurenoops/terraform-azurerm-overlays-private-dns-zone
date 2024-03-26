19f987006c82e96d85f8b4e4bf62b3756ed43961
John Spinella <jrspinella@users.noreply.github.com>
2024-03-26 15:22:16 -0400

    fix naming

--- module.dns.scaffolding.tf
+++ module.dns.scaffolding.tf
@@ -5,7 +5,7 @@
 # Azure Region Lookup
 #--------------------------------------
 # This module will lookup the Azure Region and return the short name for the region
-module "mod_azregions" {
+module "mod_azure_region_lookup" {
   source  = "azurenoops/overlays-azregions-lookup/azurerm"
   version = "~> 1.0.0"
 
